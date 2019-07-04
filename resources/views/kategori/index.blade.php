65a99de7096db5dac6a0c8c22b6e844c5e674fc4
mochammadfariz <mochammadfariz11@gmail.com>
2019-07-03 21:50:56 -0700

    menampilkan data kategori

---
+++ resources/views/kategori/index.blade.php
@@ -0,0 +1,50 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">Kategori</div>
+                <div class="card-body">
+                <a href="{{route('kategori.create')}}" class="btn btn-primary btn-sm" style="margin-bottom:20px;">Tambah Kategori</a>
+                
+                <table class="table table-bordered" id="users-table">
+                
+                    <thead>
+                        <tr>
+                            <th>No</th>
+                            <th>Nama Kategori</th>
+                            <th>Edit</th>
+                            <th>Delete</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    <?php $no=1; ?>
+                    @foreach ($kategori as $item)
+                    <tr>
+                            <td><?= $no ?></td>
+                            <td>{{$item->nama_kategori}}</td>
+                            <td><a href="{{route('kategori.edit',$item->id)}}" class="btn btn-primary btn-sm" >Edit</a></td>
+                            <td><a href="{{route('kategori.destroy',$item->id)}}" class="btn btn-danger btn-sm" >Delete</a></td>
+                        </tr>
+                        <?php $no++; ?>
+                    @endforeach
+                     
+                    </tbody>
+                </table>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+
+@push('scripts')
+<script>
+$(function() {
+$('#users-table').DataTable();
+});
+</script>
+@endpush
