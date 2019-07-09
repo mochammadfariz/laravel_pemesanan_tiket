febbdf27f62f4561f144f1d12432e75c297f4abe
mochammadfariz <mochammadfariz11@gmail.com>
2019-07-09 12:21:53 +0700

    testing tambah data kategori create

--- app/Http/Controllers/KategoriController.php
+++ app/Http/Controllers/KategoriController.php
@@ -36,7 +36,7 @@ class KategoriController extends Controller
      */
     public function store(Request $request)
     {
-        echo 'gw kezel';
+        echo '<h1>Testing Tambah Data</h1>';
     }
 
     /**
